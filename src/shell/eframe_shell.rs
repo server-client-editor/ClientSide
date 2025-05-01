5b0554bd92c378d56f9aaa3ad5a14063aeff50d4
raner <42885699+ran-err@users.noreply.github.com>
2025-05-01 17:53:17 +0800

    feat(gui): add Vec-based message bus and `update` skeleton

--- src/shell/eframe_shell.rs
+++ src/shell/eframe_shell.rs
@@ -1,9 +1,9 @@
-use std::time::{Duration, Instant};
 use anyhow::{anyhow, Result};
 use eframe::egui;
+use std::time::{Duration, Instant};
 use tracing::{debug, error, info, trace, warn};
 
-const IDLE_POLLING_INTERVAL: Duration = Duration::from_secs(1);
+const IDLE_POLLING_INTERVAL: Duration = Duration::from_millis(100);
 const FAST_POLLING_INTERVAL: Duration = Duration::from_millis(16);
 const EXITING_DEADLINE: Duration = Duration::from_secs(5);
 const FAREWELL_MESSAGE_DURATION: Duration = Duration::from_secs(2);
@@ -17,6 +17,7 @@ pub enum AppState {
 
 pub struct App {
     app_state: AppState,
+    message_bus: Vec<AppMessage>,
     shutdown_signal: Option<crossbeam_channel::Receiver<()>>,
     polling_interval: Duration,
 }
@@ -25,6 +26,7 @@ impl App {
     pub fn new() -> App {
         App {
             app_state: AppState::Running,
+            message_bus: Vec::new(),
             shutdown_signal: None,
             polling_interval: IDLE_POLLING_INTERVAL,
         }
@@ -56,6 +58,52 @@ impl App {
     }
 }
 
+pub enum AppMessage {
+    Quit,
+    PlaceHolder,
+}
+
+impl App {
+    pub fn poll_internal_events(&mut self) -> Vec<AppMessage> {
+        let mut messages = Vec::new();
+        let now = Instant::now();
+
+        match self.app_state {
+            AppState::Exiting(deadline) => {
+                if now >= deadline {
+                    messages.push(AppMessage::Quit);
+                }
+            }
+            AppState::ReadyToExit(deadline) => {
+                if now >= deadline {
+                    messages.push(AppMessage::Quit);
+                }
+            }
+            _ => {}
+        }
+
+        messages
+    }
+
+    pub fn receive_messages(&mut self, messages: &mut Vec<AppMessage>) {
+        self.message_bus.append(messages);
+    }
+
+    pub fn update(&mut self) {
+        let mut message_bus = std::mem::take(&mut self.message_bus);
+        for message in message_bus.drain(..) {
+            self.update_one(message).unwrap(); // Is dropping messages a good idea?
+        }
+    }
+
+    fn update_one(&mut self, message: AppMessage) -> Result<()> {
+        match message {
+            AppMessage::PlaceHolder => Ok(()),
+            AppMessage::Quit => Ok(()),
+        }
+    }
+}
+
 // View block
 impl App {
     pub fn view(&mut self, ctx: &egui::Context) {
@@ -73,7 +121,10 @@ impl App {
                     .resizable(false)
                     .anchor(egui::Align2::CENTER_CENTER, [0.0, 0.0])
                     .show(ctx, |ui| {
-                        ui.label(format!("Cleaning up... The application will close in {} seconds.", (deadline - now).as_secs()));
+                        ui.label(format!(
+                            "Cleaning up... The application will close in {} seconds.",
+                            (deadline - now).as_secs()
+                        ));
                     });
             }
             AppState::ReadyToExit(deadline) => {
@@ -108,6 +159,7 @@ impl App {
     pub fn new_fatal() -> App {
         App {
             app_state: AppState::Fatal("fatal error".into()),
+            message_bus: Vec::new(),
             shutdown_signal: None,
             polling_interval: IDLE_POLLING_INTERVAL,
         }
@@ -117,22 +169,28 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let start_time = Instant::now();
+        let mut external_messages = Vec::<AppMessage>::new();
 
         // Get input
         if ctx.input(|i| i.viewport().close_requested())
             && matches!(self.app_state, AppState::Running)
         {
+            external_messages.push(AppMessage::Quit);
             if let Err(e) = self.shutdown() {
                 error!("Application shutdown failed: {}", e);
             }
             ctx.send_viewport_cmd(egui::ViewportCommand::CancelClose);
         }
 
-        // Gather application information
-
         // Pass information to app::receive_events (this populates the message bus)
+        self.receive_messages(&mut external_messages);
+
+        // Gather application information
+        let mut internal_messages = self.poll_internal_events();
+        self.receive_messages(&mut internal_messages);
 
         // Loop: update application state with app::update(message)
+        self.update();
 
         // Render UI with app::view
         self.view(ctx);
